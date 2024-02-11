5a7d9a337327dec620c1dca08c5dd134831fd3ae
Taleh Guryanov <taleh.gurjanov@gmail.com>
2024-02-11 21:43:41 +0400

    updated ManageExpense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,9 +1,66 @@
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {useCallback, useLayoutEffect} from "react";
 
-export const ManageExpense = () => {
+import {IconButton} from "../components/ui/IconButton";
+import {GlobalStyles} from "../constants/styles";
+import {AppButton} from "../components/ui/AppButton";
+
+export const ManageExpense = ({route, navigation}) => {
+  const expenseId = route.params?.expenseId;
+  const isNew = !expenseId;
+  
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isNew ? 'Add Expense' : 'Edit Expense'
+    });
+  }, [navigation, isNew]);
+  
+  const deleteHandler = useCallback(() => {
+    cancelHandler();
+  },[]);
+  
+  const cancelHandler = useCallback(() => {
+    navigation.goBack();
+  },[]);
+  
+  const confirmHandler = useCallback(() => {
+    cancelHandler();
+  },[]);
+  
   return (
-      <View>
-        <Text>Manage Expense</Text>
+      <View style={styles.container}>
+        <View style={styles.buttons}>
+          <AppButton onPress={cancelHandler} mode="flat" style={styles.button}>Cancel</AppButton>
+          <AppButton onPress={confirmHandler} style={styles.button}>{isNew ? 'Add' : 'Update'}</AppButton>
+        </View>
+        {!isNew &&
+            <View style={styles.deleteButton}>
+              <IconButton color={GlobalStyles.colors.error500} size={36} name={"trash"} onPress={deleteHandler}/>
+            </View>
+        }
       </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800
+  },
+  deleteButton: {
+    padding: 8,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center'
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    flex: 1,
+  }
+})
